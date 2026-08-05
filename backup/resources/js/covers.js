842cc59f2378fde73ec058b7facb871f73e9457f
KevinRLE <klopeze14@miumg.edu.gt>
2026-08-05 17:17:24 -0600

    Elimina carpeta backup

--- backup/resources/js/covers.js
+++
@@ -1,8 +0,0 @@
-// Efecto suave al cargar videos
-document.addEventListener("DOMContentLoaded", () => {
-  const videos = document.querySelectorAll("video");
-  videos.forEach(video => {
-    video.addEventListener("mouseenter", () => video.play());
-    video.addEventListener("mouseleave", () => video.pause());
-  });
-});
